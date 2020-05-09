ad5d3c3b4d7f12f5d69fd407b12f0efdb3215e9b
Conrad Weiser <vibrantclouds@protonmail.com>
2020-05-09 18:31:22 -0400

    N-Hentai Source (#4)
    * Pull Request adherence to NHentai source
    
    Co-authored-by: ConradWeiser <ConradWeiser@users.noreply.github.com>

---
+++ src/sources/NHentai/NHentai.ts
@@ -0,0 +1,340 @@
+import { Source } from '../Source'
+import { Manga } from '../../models/Manga/Manga'
+import { Chapter } from '../../models/Chapter/Chapter'
+import { MangaTile } from '../../models/MangaTile/MangaTile'
+import { SearchRequest } from '../../models/SearchRequest/SearchRequest'
+import { Request } from '../../models/RequestObject/RequestObject'
+import { ChapterDetails } from '../../models/ChapterDetails/ChapterDetails'
+import { Tag, TagSection } from '../../models/TagSection/TagSection'
+import { HomeSection, HomeSectionRequest } from '../../models/HomeSection/HomeSection'
+import { APIWrapper } from '../../API'
+
+const NHENTAI_DOMAIN = 'https://nhentai.net'
+
+export class NHentai extends Source {
+  constructor(cheerio: CheerioAPI) {
+    super(cheerio)
+  }
+
+  get version(): string { return '0.5' }
+  get name(): string { return 'nHentai' }
+  get description(): string { return 'Extension that pulls manga from nHentai' }
+  get author(): string {return 'Conrad Weiser'}
+  get icon(): string {return "logo.png"} // The website has SVG versions, I had to find one off of a different source
+
+  convertLanguageToCode(language: string) {
+      switch(language.toLowerCase()) {
+          case "english": return "en"
+          case "japanese": return "jp"
+          case "chinese": return "chi"
+          default: return ""
+      }
+  }
+
+  getMangaDetailsRequest(ids: string[]): Request[] {
+    let requests: Request[] = []
+    for (let id of ids) {
+      let metadata = { 'id': id }
+      requests.push(createRequestObject({
+        url: `${NHENTAI_DOMAIN}/g/${id}`,
+        metadata: metadata,
+        method: 'GET'
+      }))
+    }
+    return requests
+  }
+
+  getMangaDetails(data: any[], metadata: any[]): Manga[] {
+    let manga: Manga[] = []
+    for (let [i, response] of data.entries()) {
+      let $ = this.cheerio.load(response)
+      let info = $('[itemprop=name]')
+      let image = $('[itemprop=image]').attr('content') ?? ''
+      let title = $('[itemprop=name]').attr('content') ?? ''
+
+      // Comma seperate all of the tags and store them in our tag section 
+      let tagSections: TagSection[] = [createTagSection({ id: '0', label: 'tag', tags: [] })]
+      let tags = $('meta[name="twitter:description"]').attr('content')?.split(",") ?? []
+      for(let i = 0; i < tags.length; i++) {
+        tagSections[0].tags.push(createTag({
+            id: i.toString().trim(),
+            label: tags[i]
+        }))
+      }
+
+      // Grab the alternative titles
+      let titles = [title]
+      let altTitleBlock = $('#info')
+      let altNameTop = $('h1', altTitleBlock).text() ?? ''
+      let altNameBottom = $('h2', altTitleBlock).text() ?? ''
+      if(altNameTop) {
+        titles.push(altNameTop)
+      }
+      if(altNameBottom) {
+          titles.push(altNameBottom)
+      }
+
+      // Get the artist and language information
+      let context = $("#info-block")
+      let artist = ''
+      let language = ''
+      for(let item of $('.tag-container', context).toArray()) {
+        if($(item).text().indexOf("Artists") > -1) {
+            let temp = $("a", item).text()
+            artist = temp.substring(0, temp.indexOf(" ("))
+        }
+        else if($(item).text().indexOf("Languages") > -1) {
+            let temp = $("a", item)
+            if(temp.toArray().length > 1) {
+                let temptext = $(temp.toArray()[1]).text()
+                language = temptext.substring(0, temptext.indexOf(" ("))
+            }
+            else {
+                let temptext = temp.text()
+                language = temptext.substring(0, temptext.indexOf(" ("))
+            }
+        }
+      }
+
+      let status = 1
+      let summary = ''
+      let hentai = true                 // I'm assuming that's why you're here!
+
+      manga.push(createManga({
+        id: metadata[i].id,
+        titles: titles,
+        image: image,
+        rating: 0,
+        status: status,
+        artist: artist,
+        tags: tagSections,
+        description: summary,
+        hentai: hentai
+      }))
+    }
+    return manga
+  }
+
+  getChaptersRequest(mangaId: string): Request {
+    let metadata = { 'id': mangaId }
+    return createRequestObject({
+      url: `${NHENTAI_DOMAIN}/g/${mangaId}`,
+      method: "GET",
+      metadata: metadata
+    })
+  }
+
+  getChapters(data: any, metadata: any): Chapter[] {
+    let $ = this.cheerio.load(data)
+    let chapters: Chapter[] = []
+
+    // NHentai is unique, where there is only ever one chapter.
+    let title = $('[itemprop=name]').attr('content') ?? ''
+    let time = new Date($('time').attr('datetime') ?? '')
+
+    // Get the correct language code
+    let language = ''
+    for(let item of $('.tag-container').toArray()) {
+        if($(item).text().indexOf("Languages") > -1) {
+            let temp = $("a", item)
+            if(temp.toArray().length > 1) {
+                let temptext = $(temp.toArray()[1]).text()
+                language = temptext.substring(0, temptext.indexOf(" ("))
+            }
+            else {
+                let temptext = temp.text()
+                language = temptext.substring(0, temptext.indexOf(" ("))
+            }
+        }
+      }
+
+    
+    chapters.push(createChapter({
+      id: "1",                                    // Only ever one chapter on this source
+      mangaId: metadata.id,
+      name: title,
+      chapNum: 1,
+      time: time,
+      langCode: this.convertLanguageToCode(language),
+    }))
+    return chapters
+  }
+
+  getChapterDetailsRequest(mangaId: string, chapId: string): Request {
+    let metadata = { 'mangaId': mangaId, 'chapterId': chapId}
+    return createRequestObject({
+      url: `${NHENTAI_DOMAIN}/g/${mangaId}`,
+      metadata: metadata,
+      method: 'GET',
+    })
+  }
+
+  getChapterDetails(data: any, metadata: any): { 'details': ChapterDetails, 'nextPage': boolean, 'param': string | null } {
+    let $ = this.cheerio.load(data)
+
+    // Get the number of chapters, we can generate URLs using that as a basis
+    let pages: string[] = []
+    let thumbContainer = $("#thumbnail-container")
+    let numChapters = $('.thumb-container',thumbContainer).length
+
+    // Get the gallery number that it is assigned to
+    let gallerySrc = $('img', thumbContainer).attr('data-src')
+    
+    // We can regular expression match out the gallery ID from this string
+    let galleryId = parseInt(gallerySrc?.match(/.*\/(\d*)\//)![1])
+    
+    // Grab the image thumbnail, so we can determine whether this gallery uses PNG or JPG images
+    let imageType = $('[itemprop=image]').attr('content')?.match(/cover.([png|jpg]*)/)![1]
+
+
+    /**
+     * N-Hentai always follows the following formats for their pages:
+     * https://i.nhentai.net/galleries/43181/10.png
+     * The first digit is the gallery ID we retrieved above, whereas the second is the page number.
+     * We have the image types from the thumbnail
+     */
+
+     for(let i = 1; i <= numChapters; i++) {
+        pages.push(`https://i.nhentai.net/galleries/${galleryId}/${i}.${imageType}`)
+     }
+
+    let chapterDetails = createChapterDetails({
+      id: metadata.chapterId,
+      mangaId: metadata.mangaId,
+      pages, longStrip: false
+    })
+
+    let returnObject = {
+      'details': chapterDetails,
+      'nextPage': metadata.nextPage,
+      'param': null
+    }
+
+    return returnObject
+  }
+
+
+  searchRequest(query: SearchRequest, page: number): Request | null {
+
+    // If the search query is a six digit direct link to a manga, create a request to just that URL and alert the handler via metadata
+    if(query.title?.match(/\d{6}/)) {
+      return createRequestObject({
+        url: `${NHENTAI_DOMAIN}/g/${query.title}`,
+        metadata: {sixDigit: true},
+        timeout: 4000,
+        method: "GET"
+      })
+    }
+
+    // Concat all of the available options together into a search keyword which can be supplied as a GET request param
+    let param = ''
+    if(query.title){
+        param += query.title + ' '
+    }
+    if(query.includeContent) {
+        for(let content in query.includeContent) {
+            param += ('tag:"' + query.includeContent[content] + '" ')
+        }
+    }
+    if(query.excludeContent) {
+        for(let content in query.excludeContent) {
+            param += ('-tag:"' + query.excludeContent[content] + '" ')
+        }
+    }
+
+    if(query.artist) {
+      param += ("Artist:" + query.artist + " ")
+    }
+
+    return createRequestObject({
+      url: `${NHENTAI_DOMAIN}/search/?q=${param}`,
+      metadata: query,
+      timeout: 4000,
+      method: "GET"
+    })
+  }
+
+  search(data: any): MangaTile[] {
+
+    let $ = this.cheerio.load(data)
+    let mangaTiles: MangaTile[] = []
+
+    // Was this a six digit request? We can check by seeing if we're on a manga page rather than a standard search page -- Metadata for hentai only exists on specific results, not searches, use that
+    let title = $('[itemprop=name]').attr('content') ?? ''
+    if(title) {
+      // Retrieve the ID from the body
+      let contextNode = $('#bigcontainer')
+      let href = $('a', contextNode).attr('href')
+
+      let mangaId = parseInt(href?.match(/g\/(\d*)\/\d/)![1])
+      
+      mangaTiles.push({
+        id: mangaId.toString(),
+        title: createIconText({text: $('[itemprop=name]').attr('content') ?? ''}),
+        image: $('[itemprop=image]').attr('content') ?? ''
+      })
+      return mangaTiles
+    }
+
+    let containerNode = $('.index-container')
+    for(let item of $('.gallery', containerNode).toArray()) {
+        let currNode = $(item)
+        let image = $('img', currNode).attr('data-src')!
+
+        // If image is undefined, we've hit a lazyload part of the website. Adjust the scraping to target the other features
+        if(image == undefined) {
+            image = 'http:' + $('img', currNode).attr('src')!
+        }
+
+
+        let title = $('.caption', currNode).text()
+        let idHref = $('a', currNode).attr('href')?.match(/\/(\d*)\//)!
+        
+        mangaTiles.push( {
+            id: idHref[1],
+            title: createIconText({text: title}),
+            image: image
+        })
+    }
+    
+    return mangaTiles
+  }
+  
+  getHomePageSectionRequest(): HomeSectionRequest[] | null { 
+
+    let request = createRequestObject({ url: `${NHENTAI_DOMAIN}`, method: 'GET', })
+    let homeSection = createHomeSection({ id: 'latest_hentai', title: 'LATEST HENTAI' })
+    return [createHomeSectionRequest({request: request, sections: [homeSection]})]
+    
+  }
+
+  getHomePageSections(data: any, section: HomeSection[]): HomeSection[] | null { 
+    let updatedHentai: MangaTile[] = []
+    let $ = this.cheerio.load(data)
+
+    let containerNode = $('.index-container')
+    for(let item of $('.gallery', containerNode).toArray()) {
+        let currNode = $(item)
+        let image = $('img', currNode).attr('data-src')!
+
+        // If image is undefined, we've hit a lazyload part of the website. Adjust the scraping to target the other features
+        if(image == undefined) {
+            image = 'http:' + $('img', currNode).attr('src')!
+        }
+
+        let title = $('.caption', currNode).text()
+        let idHref = $('a', currNode).attr('href')?.match(/\/(\d*)\//)!
+        
+        updatedHentai.push( {
+            id: idHref[1],
+            title: createIconText({text: title}),
+            image: image
+        })
+    }
+
+    section[0].items = updatedHentai
+    return section
+  }
+
+  
+}
---
+++ src/tests/NHentai.test.ts
@@ -0,0 +1,144 @@
+import { Source } from "../sources/Source"
+import cheerio from 'cheerio'
+import { APIWrapper } from "../API"
+import { NHentai } from "../sources/NHentai/NHentai";
+
+describe('N-Hentai Tests', function () {
+
+    var wrapper: APIWrapper = new APIWrapper();
+    var source: Source = new NHentai(cheerio);
+    var chai = require('chai'), expect = chai.expect, should = chai.should();
+    var chaiAsPromised = require('chai-as-promised');
+    chai.use(chaiAsPromised);
+
+    /**
+     * The Manga ID which this unit test uses to base it's details off of.
+     * Try to choose a manga which is updated frequently, so that the historical checking test can 
+     * return proper results, as it is limited to searching 30 days back due to extremely long processing times otherwise.
+     */
+    var hentaiId = "312468";  // Lol, it's actually called this in the href? Apparently.
+
+    it("Retrieve Manga Details", async () => {
+        let details = await wrapper.getMangaDetails(source, [hentaiId]);
+        expect(details, "No results found with test-defined ID [" + hentaiId + "]").to.be.an('array');
+        expect(details).to.not.have.lengthOf(0, "Empty response from server");
+
+        // Validate that the fields are filled - Note that there are no artists on Manganelo that I can tell
+        let data = details[0];
+        expect(data.id, "Missing ID").to.be.not.empty;
+        expect(data.image, "Missing Image").to.be.not.empty;
+        expect(data.artist, "Missing Artist").to.be.not.empty;
+        expect(data.hentai, "Missing Hentai").to.exist
+    });
+
+    it("Get Chapters", async () => {
+        let data = await wrapper.getChapters(source, hentaiId);
+        expect(data, "No chapters present for: [" + hentaiId + "]").to.not.be.empty;
+    });
+
+    it("Get Chapter Details", async () => {
+
+        let chapters = await wrapper.getChapters(source, hentaiId);
+        let data = await wrapper.getChapterDetails(source, hentaiId, chapters[0].id);
+
+        expect(data, "No server response").to.exist;
+        expect(data, "Empty server response").to.not.be.empty;
+
+        expect(data.id, "Missing ID").to.be.not.empty;
+        expect(data.mangaId, "Missing hentaiId").to.be.not.empty;
+        expect(data.pages, "No pages present").to.be.not.empty;
+    });
+
+    it("Searching for Manga With Valid Tags", async () => {
+        let testSearch = createSearchRequest({
+            title: 'female',
+            includeContent: ['bikini'],
+            excludeContent: ['sole female']
+        });
+
+        let search = await wrapper.search(source, testSearch, 1);
+        let result = search[0];
+
+        expect(result, "No response from server").to.exist;
+
+        expect(result.id, "No ID found for search query").to.be.not.empty;
+        expect(result.image, "No image found for search").to.be.not.empty;
+        expect(result.title, "No title").to.be.not.null;
+        expect(result.subtitleText, "No subtitle text").to.be.not.null;
+        expect(result.primaryText, "No primary text").to.be.not.null;
+        expect(result.secondaryText, "No secondary text").to.be.not.null;
+
+    });
+
+    it("Searching for Manga With A Valid six-digit query", async () => {
+        let testSearch = createSearchRequest({
+            title: '312483',
+        });
+
+        let search = await wrapper.search(source, testSearch, 1);
+        let result = search[0];
+        expect(result).to.exist
+
+        expect(result.id).to.exist
+        expect(result.image).to.exist
+        expect(result.title).to.exist
+    });
+
+    it("Searching for Manga With an invalid six-digit query", async () => {
+        let testSearch = createSearchRequest({
+            title: '999999',
+        });
+
+        let search = await wrapper.search(source, testSearch, 1);
+        let result = search[0];
+        expect(result).to.not.exist;    // There should be no entries with this tag!
+    });
+
+    it("Searching for Manga With Invalid Tags", async () => {
+        let testSearch = createSearchRequest({
+            title: 'Ratiaion House',
+            excludeDemographic: ['Seinen']
+        });
+
+        let search = await wrapper.search(source, testSearch, 1);
+        let result = search[0];
+        expect(result).to.not.exist;    // There should be no entries with this tag!
+    });
+
+
+    it("Searching for Manga by artist", async() => {
+        let testSearch = createSearchRequest({
+            artist: 'shiraichigo'
+        })
+
+        let search = await wrapper.search(source, testSearch, 1)
+        let result = search[0]
+        expect(result).to.exist
+    })
+
+    it("Searching for Manga by invalid artist", async() => {
+        let testSearch = createSearchRequest({
+            artist: 'Daniel Kovalevich'
+        })
+
+        let search = await wrapper.search(source, testSearch, 1)
+        let result = search[0]
+
+        expect(result).to.not.exist
+    })
+
+
+    it("Retrieve Home Page Sections", async () => {
+
+        let data = await wrapper.getHomePageSections(source);
+        expect(data, "No response from server").to.exist;
+        expect(data, "No response from server").to.be.not.empty;
+
+        // Do some MangaPark specific validation for this server response
+        let newHentai = data[0];
+        expect(newHentai.id, "Popular Titles ID does not exist").to.not.be.empty;
+        expect(newHentai.title, "Popular manga section does not exist").to.not.be.empty;
+        expect(newHentai.items, "No items available for popular titles").to.not.be.empty;
+
+    });
+});
