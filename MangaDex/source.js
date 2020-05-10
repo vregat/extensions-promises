0f446a039e20c59ceeee6a985eef64cdecf6bb48
FaizanDurrani <faizandurrani324@gmail.com>
2020-05-10 22:07:54 +0000

    Update gh-pages to output generated at abdce27

--- MangaDex/source.js
+++ MangaDex/source.js
@@ -57,7 +57,7 @@ class MangaDex extends Source_1.Source {
     constructor(cheerio) {
         super(cheerio);
     }
-    get version() { return '1.0.2'; }
+    get version() { return '1.0.3'; }
     get name() { return 'MangaDex'; }
     get icon() { return 'icon.png'; }
     get author() { return 'Faizan Durrani'; }
