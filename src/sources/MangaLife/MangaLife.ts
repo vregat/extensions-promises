d3e82823baa0d87f62148db63263c85d20edfe7e
DanielKovalevich <dkoval526@gmail.com>
2020-05-14 18:32:05 -0400

    Updated Version

--- src/sources/MangaLife/MangaLife.ts
+++ src/sources/MangaLife/MangaLife.ts
@@ -16,7 +16,7 @@ export class MangaLife extends Source {
     super(cheerio)
   }
 
-  get version(): string { return '0.5.3' }
+  get version(): string { return '0.5.4' }
   get name(): string { return 'Manga4Life' }
   get icon(): string { return 'icon.png' }
   get author(): string { return 'Daniel Kovalevich' }
