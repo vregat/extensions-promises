5c60638cafe64b90c30913eb5e5c9456f9d1c66c
Faizan Durrani <faizandurrani324@gmail.com>
2020-07-15 16:07:35 +0500

    updated paperback-extensions-common; moved mangaupdate model to commons repo

--- src/models/MangaUpdate/MangaUpdate.ts
+++
@@ -1,8 +0,0 @@
-export interface MangaUpdates {
-    ids: string[]
-    moreResults?: boolean
-}
-
-declare global {
-    function createMangaUpdates(update: MangaUpdates): MangaUpdates
-}
--- src/models/MangaUpdate/MangaUpdate_impl.ts
+++
@@ -1,7 +0,0 @@
-import { MangaUpdates } from "./MangaUpdate"
-
-const _global = global as any
-
-_global.createMangaUpdates = function (update: MangaUpdates): MangaUpdates {
-    return update
-}
